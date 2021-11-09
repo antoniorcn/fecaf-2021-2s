3b9d9d001ada5544082ab323aa6eeb24aa235e9f
Antonio Carvalho <antoniorcn@hotmail.com>
2021-11-09 08:34:35 -0300

    PDM Manha Aula 11 Parte 1

--- pdm-noite/aula11/pets/App.js
+++ pdm-noite/aula11/pets/App.js
@@ -3,6 +3,37 @@ import {View, Image, Text, ImageBackground} from 'react-native';
 import petImage from './assets/pets.png';
 
 
+const PetSummary = ({nome, raca, peso, nascimento}) => { 
+
+  // const {nome, raca, peso, nascimento} = props
+
+  return (
+    <View 
+        style = {{
+          flex: 1,
+          backgroundColor: "lightyellow",
+          justifyContent: "center"
+        }}>
+        <Text style={{
+          fontWeight: "bold"
+        }}>
+
+          {/* {props.nome} */}
+          {nome}
+
+        </Text>
+        <Text
+        style={{
+          color: "gray"
+        }}
+        >
+          {/* {props.raca} - {props.peso} kilos - {props.nascimento} */}
+          {raca} - {peso} kilos - {nascimento}
+          </Text>
+    </View>
+  )
+}
+
 
 export default () => { 
 
@@ -25,8 +56,8 @@ export default () => {
               padding: 30,
               alignItems: "center"
             }}
-            // source={petImage}
-            source={{uri: "https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png"}}
+            source={petImage}
+            // source={{uri: "https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png"}}
             > 
             <Text style={{
               color : "pink",
@@ -43,7 +74,16 @@ export default () => {
         <View style={{
           flex: 2,
           backgroundColor: "yellow"
-        }}></View>
+        }}>
+          <PetSummary nome="Mike" raca="Viralata" peso="11,4" nascimento="25/01/2014"/>
+
+          <PetSummary nome="Princesa" raca="Poodle" peso="7,6" nascimento="10/06/2018"/>
+
+          <PetSummary nome="Rex" raca="Pastor Alemão" peso="24,6" nascimento="05/04/2017"/>
+
+          <PetSummary nome="Totó" raca="Bulldog" peso="12,7" nascimento="10/05/2015"/>
+
+        </View>
 
       <View></View>
 
